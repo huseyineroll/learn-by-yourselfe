1ecbc2973591fa813bf41aaaf25c2ed951d41165
apple <hsyn.erol56@gmail.com>
2022-08-19 20:44:11 +0300

    added a cute pipull pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from "react"
 import Layout from '../compenets/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
-const HomePage = () => { 
+const HomePage = () => {
   return (
-    <Layout pageHeading='Home' pageTitle='Site Home'>
-      Welcome to my cool Gatsby Site.
- </Layout> 
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+    </Layout>
   )
- }
-
- export default HomePage
+}
+export const Head = () => <title>Home Page</title>
+export default HomePage
